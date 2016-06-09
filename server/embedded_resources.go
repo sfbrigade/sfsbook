9b420aa76119ff448a09b1b18fc0461a16a5c48a
Robert Kroeger <rjkroege@liqui.org>
2016-06-09 07:11:32 -0700

    Added basic logic to serve from compiled in or site
    If there is a file in the CWD/site directory, use that. Otherwise, serve
    a resource compiled into the program.

---
+++ server/embedded_resources.go
@@ -0,0 +1,17 @@
+package server
+
+
+var Resources map[string]string = map[string]string{
+	"index.html": `<html>
+<body>
+Hello from sfsbook!
+</body>
+</html>`,
+
+	"embedded.html": `<html>
+<body>
+This is an embedded resource
+</body>
+</html>`,
+}
+
--- server/static.go
+++ server/static.go
@@ -7,7 +7,10 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"strings"
 )
+
+//go:generate go run ../generator/generateresources.go ../sites
 
 // TODO(rjk): This will probably require additional fields.
 type staticServer struct {
@@ -19,34 +22,48 @@ func MakeStaticServer(pathroot string) *staticServer {
 }
 
 func (gs *staticServer) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	// TODO(rjk): This doesn't handle sub-directories correctly.
 	_, sn := filepath.Split(req.URL.Path)
 	if sn == "" {
 		sn = "index.html"
 	}
-
 	filepath := filepath.Join(gs.s, sn)
+
+	var resreader  io.Reader
 
 	log.Println(filepath)
 	if _, err := os.Stat(filepath); err != nil {
-		log.Println("file", filepath, "missing", err, "ought to be trying the wired-in content")
-		// TODO(rjk): If the file doesn't exist, we default to using the content
-		// compiled into the application.
-		respondWithError(w, fmt.Sprintln("Can't open file: ", err))
-		return
+		res, ok := Resources[sn]
+		if !ok {
+			log.Println("file", filepath, "missing from disk", err, "and also missing", sn, "from compiled in resource")
+			respondWithError(w, fmt.Sprintln("Can't open file: ", err))
+			return
+		}
+	
+		resreader = strings.NewReader(res)
+	} else {
+		f, err := os.Open(filepath)
+		// Set up processing chain for file here. There are two kinds of processing. Static
+		// (i.e. processing that happens at generate time) and dynamic (templating) that
+		// happen at serve time.
+
+		// TODO(rjk): I'm not sure that we necessarily want to expose our innards
+		// like this. Perhaps I need to revise my opinions about how the error handling
+		// should work.
+		if err != nil {
+			log.Println("problem opening file", filepath, err)
+			respondWithError(w, fmt.Sprintln("Can't open file: ", err))
+			return
+		}
+		defer f.Close()
+		resreader = f		
 	}
 
-	f, err := os.Open(filepath)
-	defer f.Close()
-	// TODO(rjk): I'm not sure that we necessarily want to expose our innards
-	// like this. Perhaps I need to revise my opinions about how the error handling
-	// should work.
-	if err != nil {
-		log.Println("problem opening file", filepath, err)
-		respondWithError(w, fmt.Sprintln("Can't open file: ", err))
-		return
-	}
+	// What about templating? Or generative content? We need to insert a phase here
+	// here (some refactoring required) that can re-process the file.
 
-	if _, err := io.Copy(w, f); err != nil {
+	// Processing of the content has to happen here.	
+	if _, err := io.Copy(w, resreader); err != nil {
 		log.Println("could not copy to the request body ", err)
 		respondWithError(w, fmt.Sprintln("Can't copy: ", err))
 		return
