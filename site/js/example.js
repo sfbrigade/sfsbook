b809e592f67469a18f49f334e1f8a04d52095091
Robert Kroeger <rjkroege@users.noreply.github.com>
2016-10-15 10:44:11 -0700

    Make sign-out work (#62)
    Added sign-out functionality.

--- site/js/example.js
+++ site/js/example.js
@@ -5,3 +5,9 @@ function addSomething() {
 	p.innerText = "Something here. But not very exciting";
 	el.appendChild(p)
 }
+
+// Delete the session cookie and reload the page in the unauthed state.
+function clearSessionCookie() {
+	document.cookie = "session=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/";
+	window.location.reload();
+}
