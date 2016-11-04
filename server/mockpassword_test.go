2547e1e91bb6fa0bacbb00c31d7405cb9d1ad543
Robert Kroeger <rjkroege@liqui.org>
2016-11-03 19:00:14 -0700

    Added tests for user management
    Implemented a higher-level abstraction for access to the password file.
    Wrote a unit test for user management on top of the new abstraction.

---
+++ server/mockpassword_test.go
@@ -0,0 +1,61 @@
+package server
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/blevesearch/bleve"
+	"github.com/rjkroege/mocking"
+)
+
+// TODO(rjk): Complete the higher level of semantic abstraction with 
+// some kind of appropriate wrapper for Search.
+type mockPasswordIndex mocking.Tape
+
+type indexStim struct {
+	fun  string
+	id   string
+	data interface{}
+}
+
+type docStim struct {
+	name string
+	uuid string
+}
+
+func (mpi *mockPasswordIndex) Index(id string, data interface{}) error {
+	tape := (*mocking.Tape)(mpi)
+	res := tape.Record(indexStim{
+		"PasswordIndex.Index",
+		id,
+		data,
+	})
+	switch v := res.(type) {
+	case nil:
+		return nil
+	case error:
+		return v
+	}
+	log.Fatalf("%s (mock) response %v is of bad type", "PasswordIndex.Index", res)
+	return nil
+}
+
+func (tape *mockPasswordIndex) Search(_ *bleve.SearchRequest) (*bleve.SearchResult, error) {
+	return nil, fmt.Errorf("not-implemented")
+}
+
+func (mpi *mockPasswordIndex) MapForDocument(id string) (map[string]interface{}, error) {
+	tape := (*mocking.Tape)(mpi)
+	res := tape.Record(docStim{
+		"PasswordIndex.Document",
+		id,
+	})
+	switch v := res.(type) {
+	case map[string]interface{}:
+		return v, nil
+	case error:
+		return nil, v
+	}
+	log.Fatalf("%s (mock) response %v is of bad type", "PasswordIndex.Document", res)
+	return nil, nil
+}
