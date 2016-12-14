f7472916b1e92b81decd0f9564f0f29c6b54d457
Claire Hsu <clairehsu86@gmail.com>
2016-12-14 11:27:00 -0800

    tidy

--- site/js/searchbar.js
+++ site/js/searchbar.js
@@ -2,61 +2,62 @@
 
 // toggleCategoryOption adds or removes a keyword from the search
 function toggleCategoryOption() {
-	var el = document.getElementById("query_field");
-        var searchText = el.value
-	var optionValue = this.children[0].value;
-	var re = new RegExp("\\b(" + optionValue + ")\\b", "gi");
-        var stringToReplace = optionValue.concat(", ");
-	if(searchText.match(stringToReplace)) {
-			var stringToReplace = optionValue.concat(", ");
-			searchText = searchText.replace(stringToReplace, "");
-			el.value = searchText;
-		} else if (searchText.match(re)) {
-			searchText = searchText.replace(optionValue, "");
-			el.value = searchText;
-		} else {
-		if(searchText.length > 0){
-			el.value = searchText.concat(", ",this.children[0].value)
-		} else {
-			el.value = optionValue;
-		}
-	}
+  var el = document.getElementById('query_field');
+  var searchText = el.value;
+  var optionValue = this.children[0].value;
+  var re = new RegExp('\\b(' + optionValue + ')\\b', 'gi');
+  var stringToReplace = optionValue.concat(', ');
+  if(searchText.match(stringToReplace)) {
+    stringToReplace = optionValue.concat(', ');
+    searchText = searchText.replace(stringToReplace, '');
+    el.value = searchText;
+  } else if (searchText.match(re)) {
+    searchText = searchText.replace(optionValue, '');
+    el.value = searchText;
+  } else {
+    if(searchText.length > 0){
+      el.value = searchText.concat(', ',this.children[0].value);
+    } else {
+      el.value = optionValue;
+    }
+  }
 }
 
 // addEventListener attaches toggleCategoryOption to the checkboxes
 function addEventListener(el, eventName, handler) {
-	eventName.preventDefault ? eventName.preventDefault() : (eventName.returnValue = false);
-  	if (el.addEventListener) {
-    		el.addEventListener(eventName, handler);
-  	} else {
-    		el.attachEvent('on' + eventName, function(){
-      			handler.call(el);
-    		});
-  	}
+  eventName.preventDefault ? eventName.preventDefault() : (eventName.returnValue = false);
+  if (el.addEventListener) {
+    el.addEventListener(eventName, handler);
+  } else {
+    el.attachEvent('on' + eventName, function(){
+      handler.call(el);
+    });
+  }
 }
 
 // attachToggles attaches event listener to each category option
 function attachToggles() {
-	var categories = document.getElementsByClassName("category-option");
-	for(var i = 0; i < categories.length; i++){
-		el = categories[i];
-		addEventListener(el, 'click', toggleCategoryOption);
-	}
+  var categories = document.getElementsByClassName('category-option');
+  for(var i = 0; i < categories.length; i++){
+    el = categories[i];
+    addEventListener(el, 'click', toggleCategoryOption);
+  }
 }
 
 // ready calls addEventListener once the page has loaded
 function ready(fn) {
-  	if (document.readyState != 'loading'){
-    		fn();
-  	} else if (document.addEventListener) {
-    		document.addEventListener('DOMContentLoaded', fn);
-  	} else {
-    		document.attachEvent('onreadystatechange', function() {
-      		if (document.readyState != 'loading')
-        		fn();
-    		});
-  	}
+  if (document.readyState != 'loading'){
+    fn();
+  } else if (document.addEventListener) {
+    document.addEventListener('DOMContentLoaded', fn);
+  } else {
+    document.attachEvent('onreadystatechange', function() {
+      if (document.readyState != 'loading') {
+        fn();
+      }  
+    });
+  }
 }
 
-//calls ready with attachToggles
+//calls ready function with attachToggles
 ready(attachToggles);
