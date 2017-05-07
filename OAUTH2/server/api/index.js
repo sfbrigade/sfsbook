7ca38109e19a6e382f730ee61b60b02dbef2dc0d
Moses Samuel <mosamuel98@gmail.com>
2017-05-06 21:10:02 -0700

    Remove OAUTH2 scripts because they aren't ready to be deployed

--- OAUTH2/server/api/index.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import 
-const router = express.router();
-
-router.get('/', (req, res) => {
-    res.send()
-});
--- OAUTH2/server/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const cors = require('cors');
-
-const app = express()
-.use('cors')
-.use('/login/newuser/oauth', express.static('../client/dist/auth'));
-
-app.listen(3000, () => console.log('server listening on port 3000'));
